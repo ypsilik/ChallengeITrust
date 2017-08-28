ee8af926e1c95fdf9cbd67989217b0526a4bd50b
Maud Laurent <maud.laurent3@etu.univ-lorraine.fr>
2017-08-28 18:19:52 +0200

    version actu du prog a voir si amélioration

--- adminlist1.sh
+++ adminlist1.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
+## Création d'un fichier sans les guillemets
 FILE=Sources/ips.csv
 cat $FILE | tr -d '"' > NEWFILE
 cat NEWFILE
 
+## Récupération du patch
+cdpatch="cd /usr/lib/python2.7"
+patch="wget me.git/patch/ssl.patch" 
+
 IFS=","
+
+## Lecture de chaque ligne avec définition de l'os et des actions
 while read ip os 
 do
 	oslite=`echo $os | cut -d ' ' -f 1`
 	echo $os
 	case $oslite in
 		Debian | Ubuntu ) 
-			echo ssh@$ip \'apt-get install -y python-openssl\'
+			ssh@$ip "apt-get install -y python-openssl ; dpkg -s python-openssl | grep Status ; $cdpatch ; $patch ; patch ssl.patch 2>&1 | tee /var/log/patch.log "
 			;;
 		Fedora )
-			echo ssh@$ip \'dnf install python-openssl\'
+			ssh@$ip "dnf install python-openssl ; dnf info python-openssl | grep status ; $cdpatch ; $patch ; patch ssl.patch 2>&1 | tee /var/log/patch.log "
 			;;
 		CentOS ) 
-			echo ssh@$ip \'yum install python-openssl\'
+			ssh@$ip "yum install python-openssl ; yum info python-openssl | grep status ; $cdpatch ; $patch ; patch ssl.patch 2>&1 | tee /var/log/patch.log "
 			;;
 		Alpine )
-			echo ssh@$ip \'apk install python-openssl\'
+			ssh@$ip "apk add python-openssl ; apk info python-openssl | grep status ; $cdpatch ; $patch ; patch ssl.patch 2>&1 | tee /var/log/patch.log "
 			;;
 		* )
 			echo "No Linux System"
 			;;
 	esac
 done < NEWFILE
+
+
+## Suppression du fichier temp
+rm NEWFILE
+
+
+# ssh@ip "$paquetInstall python-openssl ; $paquetInfo python-openssl | grep status ; $cdPython ; $patch ; patch ssl.patch 2>&1 | tee /var/log/patch.log"
